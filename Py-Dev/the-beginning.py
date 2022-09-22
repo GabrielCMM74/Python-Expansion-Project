b16a096f8b2ee565427edf14295ba6b01d0f80cf
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2022-09-21 21:46:08 -0400

    Easy Material

---
+++ Py-Dev/the-beginning.py
@@ -0,0 +1,184 @@
+#  Terminal Commands
+
+# picture = [
+#     [0, 0, 0, 1, 0, 0, 0],
+#     [0, 0, 1, 1, 1, 0, 0],
+#     [0, 1, 1, 1, 1, 1, 0],
+#     [1, 1, 1, 1, 1, 1, 1],
+#     [0, 0, 0, 1, 0, 0, 0],
+#     [0, 0, 0, 1, 0, 0, 0]
+# ]
+# for image in picture:
+#     for pixel in image:
+#         if (pixel):
+#             print('*', end="")
+#         else:
+#             print('  ', end="")
+#         print('')
+
+
+# def multiply_by_two(num):
+#     return num * 2
+
+
+# def sum(num1, num2):
+#     return num1 + num2
+
+
+# print(sum(20, 3))
+
+
+# name =input('What is your name?')
+
+# print('Hello  ' + name)
+
+print(type(2*4))
+
+# Modular % the remainder between two 
+
+#bin returns the binary number 
+
+# String indexes [Specific Letter]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
