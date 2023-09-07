efb5a8a7ec7b5c17cd78b16f3861c53d4122f96b
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2023-09-07 09:09:51 -0400

    Section 14 regex done

---
+++ section14.py
@@ -0,0 +1,8 @@
+import re
+# REGEX
+
+string = 'search inside here'
+print(re.search('search', string))
+# pattern search findall, full match.
+# regex playground
+pw = re.compile(r"[a-zA-Z0-9$%#@]{8,}\d")
