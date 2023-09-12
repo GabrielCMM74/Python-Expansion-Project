af9bdc74a2f7b1cb00d92b90925872be0dcb5624
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2023-09-12 05:36:34 -0400

    COmpleted section 14

--- section14.py
+++ section14.py
@@ -6,3 +6,6 @@ print(re.search('search', string))
 # pattern search findall, full match.
 # regex playground
 pw = re.compile(r"[a-zA-Z0-9$%#@]{8,}\d")
+
+# Debugging
+# Linting helps to make sure were not making a mistake
