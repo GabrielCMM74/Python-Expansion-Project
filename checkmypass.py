21d4095228fb3dc3cfaaf6eba72c34101482b114
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2023-09-13 18:06:26 -0400

    Adding Hashing

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,20 @@
 import requests
+import hashlib
 
-url = 'https://api.pwnedpasswords.com/range/' + 'passwords'
-res = requests.get(url)
-print(res)
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error Fetching: {res.status_code}, checking')
+    return res
+
+
+def pwned_api_check(password):
+    # Check if it is in API
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    # sha1password = hashlib.sha1(password.encode('utf-8'))
+    return sha1password
+
+
+pwned_api_check('123')
