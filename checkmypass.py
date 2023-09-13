5629e2903110da8c164fcc5abb4895a15a725157
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2023-09-13 18:19:14 -0400

    check password 4 adding print

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -14,6 +15,14 @@ def read_res(response):
     print(response.text)
 
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
     # Check if it is in API
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
@@ -21,7 +30,18 @@ def pwned_api_check(password):
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
     print(response)
-    return read_res(response)
+    return get_password_leaks_count(response, tail)
 
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times, change password')
+        else:
+            print(f'{password} was not found')
+
+    return 'done!'
+
+
+main(sys.argv[1:])
