6175f429f970abc17f394280c5cbd201c34500ec
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2023-09-20 19:43:14 -0400

    Almost working web scraper

--- section18.py
+++ section18.py
@@ -1,7 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
 print(res)  # res.text would give the full html
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.body)  # now this is an object and we can manipulate it
+links = soup.select('.titleline')
+subtext = soup.select('.subtext')
+href = soup.select('a')
+
+
+def create_custom_hn(links, subtext, href):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return hn
+# print(votes[0].get('id'))
+# print(soup.select('.titleline'))  # now this is an object and we can manipulate it
+
+
+pprint.pprint(create_custom_hn(links, subtext, href))
