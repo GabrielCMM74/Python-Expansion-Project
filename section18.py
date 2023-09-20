c9bb436e6555779208eb244f7f3aa2f00454a68e
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2023-09-20 19:54:18 -0400

    Fixed link error

--- section18.py
+++ section18.py
@@ -5,12 +5,12 @@ import pprint
 res = requests.get('https://news.ycombinator.com/news')
 print(res)  # res.text would give the full html
 soup = BeautifulSoup(res.text, 'html.parser')
-links = soup.select('.titleline')
+links = soup.select('.titleline > a')
 subtext = soup.select('.subtext')
 href = soup.select('a')
 
 
-def create_custom_hn(links, subtext, href):
+def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
         title = item.getText()
@@ -18,11 +18,11 @@ def create_custom_hn(links, subtext, href):
         vote = subtext[idx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
-            if points > 99:
+            if points > 50:
                 hn.append({'title': title, 'link': href, 'votes': points})
     return hn
 # print(votes[0].get('id'))
 # print(soup.select('.titleline'))  # now this is an object and we can manipulate it
 
-
-pprint.pprint(create_custom_hn(links, subtext, href))
+print(href)
+pprint.pprint(create_custom_hn(links, subtext))
