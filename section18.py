981288f6bae43e067e0cea9302b557d5c84f942d
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2023-09-20 19:00:49 -0400

    Starting Hackerrank web scrape

--- section18.py
+++ section18.py
@@ -5,4 +5,3 @@ res = requests.get('https://news.ycombinator.com/news')
 print(res)  # res.text would give the full html
 soup = BeautifulSoup(res.text, 'html.parser')
 print(soup.body)  # now this is an object and we can manipulate it
-
