244e15a637cf521076ba420b8b1d3d8468841250
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2023-10-17 06:56:33 -0400

    Added new MIT Algo Section for DSA Course

---
+++ MIT-DSA/BigO.py
@@ -0,0 +1,32 @@
+# O ( 1 )
+# Array
+nums = [1, 2, 3]
+nums.append(4)    # push to end
+nums.pop()        # pop from end
+nums[0]           # lookup
+nums[1]
+nums[2]
+
+
+# HashMap / Set
+hashMap = {}
+hashMap["key"] = 10     # insert
+print("key" in hashMap)  # lookup
+print(hashMap["key"])   # lookup
+hashMap.pop("key")      # remove
+
+# O(n)
+nums = [1, 2, 3]
+sum(nums)           # sum of array
+for n in nums:      # looping
+    print(n)
+
+nums.insert(1, 100)  # insert middle
+nums.remove(100)    # remove middle
+print(100 in nums)  # search
+
+import heapq
+heapq.heapify(nums)  # build heap
+
+# sometimes even nested loops can be O(n)
+# (e.g. monotonic stack or sliding window)
